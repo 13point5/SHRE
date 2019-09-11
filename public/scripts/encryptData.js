2396514238090df86f6c2002beb500fefd7dd1f3
Bharath <albharath.1305@gmail.com>
2019-09-11 19:57:03 +0530

    input errors handled

--- public/scripts/encryptData.js
+++ public/scripts/encryptData.js
@@ -8,13 +8,19 @@ encryptForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const plainText = plainTextField.value
 
-    fetch('/encrypt?text=' + plainText).then((response) => {
-        response.json().then((data) => {
-            if (data.error) {
-                encryptedTextField.textContent = 'Error'
-            } else {
-                encryptedTextField.textContent = data.cipherText
-            }
+    if (! /^[a-z]+$/.test(plainText)) {
+        alert("Input text can only contain lower case alphabets")
+    } else {
+
+        fetch('/encrypt?text=' + plainText).then((response) => {
+            response.json().then((data) => {
+                if (data.error) {
+                    encryptedTextField.textContent = 'Error'
+                } else {
+                    encryptedTextField.textContent = data.cipherText
+                }
+            })
         })
-    })
+
+    }
 })
